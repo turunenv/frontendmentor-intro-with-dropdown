6c90e8fcbea8ec7e4f733cccf3732108f84253d6
turunenv <turunenv@outlook.com>
2024-02-22 11:10:29 +0200

    fix element order

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -4,21 +4,23 @@ import styles from './MainPage.module.css';
 export function MainPage() {
   return (
     <main>
-      <h1>Make remote work</h1>
       <div>
-        <img 
-          src={mobileHeroImgUrl} 
-          alt="a man working on his laptop" 
+        <img
+          src={mobileHeroImgUrl}
+          alt='a man working on his laptop'
           className={styles.heroImg}
         />
       </div>
-      <div>
-        Get your team in sync, no matter your location. Streamline processes, 
-        create team rituals, and watch productivity soar.
-      </div>
-      <div>
-        <button>Learn more</button>
-      </div>
+      <section className={styles.mainContent}>
+        <h1 className={styles.mainTitle}>Make remote work</h1>
+        <p className={styles.mainDescription}>
+          Get your team in sync, no matter your location. Streamline processes,
+          create team rituals, and watch productivity soar.
+        </p>
+        <button className={styles.button}>
+            Learn more
+        </button>
+      </section>
     </main>
-  )
+  );
 }
