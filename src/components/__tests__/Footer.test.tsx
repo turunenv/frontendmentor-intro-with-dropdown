26189cbcabd81d4a4578fb9ff038fb915fbf46a4
turunenv <turunenv@outlook.com>
2024-02-23 15:11:02 +0200

    add tests for Footer

--- src/components/__tests__/Footer.test.tsx
+++ src/components/__tests__/Footer.test.tsx
@@ -1,4 +1,24 @@
-import { test, expect } from 'vitest';
+import { test, expect, beforeEach } from 'vitest';
 import { screen, render } from '@testing-library/react';
+import { Footer } from '../Footer';
+import { footerImages } from '../../content/footerItems';
 
-test('renders ')
+beforeEach(() => {
+  render(<Footer />);
+});
+
+test('renders all images', () => {
+  const expectedImgCount = footerImages.length;
+
+  const images = screen.getAllByRole('img');
+  expect(images.length).toBe(expectedImgCount);
+});
+
+test('images have expected alt-texts', () => {
+  const altTexts = footerImages.map((imgTuple) => imgTuple[1]);
+
+  altTexts.forEach((text) => {
+    const imgElement = screen.queryByAltText(text);
+    expect(imgElement).not.toBeNull();
+  });
+});
